89ac1f598a35dcf7e0ff7d7950f261ddb6c6b0a2
Yassir <elkhailiyassir@gmail.com>
2023-10-13 15:06:21 +0100

    pushed

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header/>
         {children}
-        <Footer/>
+        <Footer />
         </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./partials/Header"
 export default function Home() {
   return (
     <main>
+      Hello next
     </main>
   )
 }
